3229c6ebab20b93812bcb0ba903863e8013701bc
unknown <dominic.viehbock@gmail.com>
2019-12-06 16:04:04 +0100

    overhaul of file upload model&form

--- jobs/models.py
+++ jobs/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
 
 
 class Job(models.Model):
-    start_date = models.DateTimeField(default=timezone.now)
+    # start_date = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_finished = models.BooleanField(default=False)
-    time_finished = models.DateTimeField(default=timezone.now)
+    # is_finished = models.BooleanField(default=False)
+    # time_finished = models.DateTimeField(default=timezone.now)
+    sbml_file = models.FileField(upload_to='uploadz',
+                                 validators=[FileExtensionValidator(allowed_extensions=['sbml', 'xml'],
+                                                                    message='Wrong file type!')])
--- jobs/views.py
+++ jobs/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
-
+from .forms import JobSubmissionForm
+from django.contrib import messages
+from .models import Job
 
 @login_required
 def overview(request):
-    # send_mail('testsubject',
-    #           'message',
-    #           '',
-    #           ['dominic.viehbock@gmail.com'],
-    #           fail_silently=False,
-    #           )
-    return render(request, 'jobs/overview.html')
+    jobs = Job.objects.filter(user=request.user)
+    return render(request, 'jobs/overview.html', context={'jobs': jobs})
 
+
+@login_required
+def new(request):
+    form = JobSubmissionForm(request.POST, request.FILES or None)
+    if request.POST:
+        if form.is_valid():
+            extended = form.save(commit=False)
+            extended.user = request.user
+            extended.file = request.FILES
+            extended.save()
+
+        else:
+            messages.warning(request, message='Something went wrong')
+            print(form)
+            print(form.errors)
+            print(form.fields)
+            print(form.data)
+    return render(request, 'jobs/new.html', context={'form': form})
